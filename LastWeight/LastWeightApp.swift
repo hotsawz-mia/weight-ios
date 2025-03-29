159f963bd6a72f6310de02367f5ca4b3ac82d783
hotsawz-mia <limiter_inks.0k@icloud.com>
2025-03-29 11:27:07 +0000

    Initial Commit

---
+++ LastWeight/LastWeightApp.swift
@@ -0,0 +1,17 @@
+//
+//  LastWeightApp.swift
+//  LastWeight
+//
+//  Created by Mark James on 29/03/2025.
+//
+
+import SwiftUI
+
+@main
+struct LastWeightApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
